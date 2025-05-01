cb11d1050a49db0e9752e848592e42cdb082c888
Simone D'Amico <simone.damico@spreaker.com>
2025-05-01 11:05:55 +0200

    Added backend app

---
+++ backend/index.php
@@ -0,0 +1,36 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Tuupola\Middleware\CorsMiddleware;
+
+$app = AppFactory::create();
+
+$app->add(new CorsMiddleware([
+    "origin" => ["*"],
+    "methods" => ["GET", "PATCH", "OPTIONS"],
+    "headers.allow" => ["Content-Type", "Authorization"],
+    "headers.expose" => [],
+    "credentials" => false,
+    "cache" => 0,
+]));
+
+$app->options('/{routes:.+}', function (Request $request, Response $response) {
+    return $response;
+});
+
+$app->get('/documents/{documentId}', function (Request $request, Response $response, array $args) {
+    $payload = json_encode([]);
+    $response->getBody()->write($payload);
+    return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+});
+
+$app->patch('/documents/{documentId}/translations/{translationId}', function (Request $request, Response $response, array $args) {
+    $payload = json_encode([]);
+    $response->getBody()->write($payload);
+    return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+});
+
+$app->run();
