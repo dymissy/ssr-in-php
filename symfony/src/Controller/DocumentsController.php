dc605bcbc8facd33aff6345431b3c5ac08cadb79
Simone D'Amico <simone.damico@spreaker.com>
2025-05-02 16:19:53 +0200

    Documents list

--- symfony/src/Controller/DocumentsController.php
+++ symfony/src/Controller/DocumentsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Services\ApiClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Attribute\Route;
 final class DocumentsController extends AbstractController
 {
     #[Route('/', name: 'documents')]
-    public function index(): Response
+    public function index(ApiClient $apiClient): Response
     {
         return $this->render('documents/index.html.twig', [
-            'controller_name' => 'DocumentsController',
+            'documents' => $apiClient->getDocuments(),
         ]);
     }
 }
