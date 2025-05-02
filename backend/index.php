185310af750b33bc8a724b7af8a5306c29176c09
Simone D'Amico <simone.damico@spreaker.com>
2025-05-02 12:09:58 +0200

    Backend: multiple documents support

--- backend/index.php
+++ backend/index.php
@@ -21,8 +21,26 @@ $app->options('/{routes:.+}', function (Request $request, Response $response) {
     return $response;
 });
 
+$app->get('/documents', function (Request $request, Response $response) {
+    $documents = [];
+    foreach (glob('data/document*.json') as $filename) {
+        $data = json_decode(file_get_contents($filename), true);
+        $documents[] = $data['document'];
+    }
+
+    $payload = json_encode(['documents' => $documents]);
+    $response->getBody()->write($payload);
+    return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+});
+
 $app->get('/documents/{documentId}', function (Request $request, Response $response, array $args) {
-    $payload = file_get_contents(__DIR__ . '/data/document.json');
+    $documentId = (int) $args['documentId'];
+    if (!file_exists(__DIR__ . "/data/document$documentId.json")) {
+        $response->getBody()->write(json_encode(['error' => 'Document not found']));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(404);
+    }
+
+    $payload = file_get_contents(__DIR__ . "/data/document$documentId.json");
     $response->getBody()->write($payload);
     return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
 });
@@ -30,7 +48,7 @@ $app->get('/documents/{documentId}', function (Request $request, Response $respo
 $app->patch('/documents/{documentId}/translations/{translationId}',
     function (Request $request, Response $response, array $args) {
         $translationId = (int) $args['translationId'];
-        $data = json_decode(file_get_contents(__DIR__.'/data/document.json'), true);
+        $data = json_decode(file_get_contents(__DIR__.'/data/document1.json'), true);
 
         $filteredTranslations = array_filter($data['document']['translations'], function ($item) use ($translationId) {
             return $item['translationId'] === $translationId;
