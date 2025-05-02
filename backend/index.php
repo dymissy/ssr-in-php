77c335978a7e33af4c594641db0f50b3dfa980fd
Simone D'Amico <simone.damico@spreaker.com>
2025-05-02 10:17:44 +0200

    Backend: change status and progress on patch

--- backend/index.php
+++ backend/index.php
@@ -27,10 +27,28 @@ $app->get('/documents/{documentId}', function (Request $request, Response $respo
     return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
 });
 
-$app->patch('/documents/{documentId}/translations/{translationId}', function (Request $request, Response $response, array $args) {
-    $payload = json_encode([]);
-    $response->getBody()->write($payload);
-    return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
-});
+$app->patch('/documents/{documentId}/translations/{translationId}',
+    function (Request $request, Response $response, array $args) {
+        $translationId = (int) $args['translationId'];
+        $data = json_decode(file_get_contents(__DIR__.'/data/document.json'), true);
+
+        $filteredTranslations = array_filter($data['document']['translations'], function ($item) use ($translationId) {
+            return $item['translationId'] === $translationId;
+        });
+
+        $translation = array_values($filteredTranslations)[0] ?? null;
+        if (!$translation) {
+            $response->getBody()->write(json_encode(['error' => 'Translation not found']));
+
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(404);
+        }
+
+        $translation['status'] = 'Confirmed';
+        $translation['progress'] = 100;
+
+        $response->getBody()->write(json_encode($translation));
+
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+    });
 
 $app->run();
